242d30894563e993909bf2233d46aad70cf26d94
yss <847830169@qq.com>
2023-02-04 15:27:45 +0800

    [dir]add 3.6

---
+++ cht3_function/3_6name.py
@@ -0,0 +1,22 @@
+#局部作用域不能使用其他局部作用域的变量
+def spam():
+    eggs = 99
+    bacon()
+    print(eggs)
+
+def bacon():
+    ham = 101
+    eggs = 0
+
+spam()
+
+#全局变量可以在局部作用域中读取
+def spam():
+    print(eggs) 
+eggs = 42
+spam()
+print(eggs)
+
+
+
+
