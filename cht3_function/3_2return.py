bd1888e9981fc3ed9f5ede548e2702cd1aa96056
yss <847830169@qq.com>
2023-02-04 12:01:29 +0800

    [dir]add 3.2-3.3

---
+++ cht3_function/3_2return.py
@@ -0,0 +1,16 @@
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'Yes'
+    elif answerNumber == 3:
+        return 'Very doubtful'
+
+r = random.randint(1,3)
+fortune = getAnswer(r)
+print(fortune)
+
+#可以缩成一行
+print(getAnswer(random.randint(1,3)))
