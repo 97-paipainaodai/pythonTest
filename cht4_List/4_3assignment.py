e01a48ad4025babddd14647de845f71aaa24e855
yss <847830169@qq.com>
2023-02-05 10:59:22 +0800

    [dir]add4_3

---
+++ cht4_List/4_3assignment.py
@@ -0,0 +1,16 @@
+num = 42
+num = num +1
+num +=1
+
+num -=1
+num *=1
+num /=1
+num %=1   #等价num = num % 1
+
+spam = 'Hello,'
+spam += ' world!'
+print(spam)
+
+bacon = ['Cat']
+bacon *=3
+print(bacon)
