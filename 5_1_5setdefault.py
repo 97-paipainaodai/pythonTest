4bdd693f59f449373fd3d57342bcff31a873f307
yss <847830169@qq.com>
2023-02-11 21:58:33 +0800

    [add]dir5_1_5

--- 5_1_5setdefault.py
+++ 5_1_5setdefault.py
@@ -1,9 +1,21 @@
 #setdefult():  to set a default value for a key
 spam = {'color': 'red', 'age': 42}
 print(spam)
-if 'color' not in spam:
-    spam['color'] = 'black'
-# print(spam)
+
+if 'name' not in spam:
+    spam['name'] = 'Pooka'
+print(spam)
+
 bacon = {'color': 'red', 'age': 42}
-bacon.setdefault('color', 'black')
+bacon.setdefault('name', 'Pooka')
 print(bacon)
+bacon.setdefault('name', 'Simy')  #can not be changed twice
+print(bacon)
+
+message = 'It was a blight cold day in April.'
+count = {}
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+print(count)
