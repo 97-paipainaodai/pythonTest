f7f85c9c7f9fa47cc2010f89d746775625930d59
yss <847830169@qq.com>
2023-02-14 22:36:13 +0800

    [dir]add6_3_!

---
+++ cht6_String/6_3_1upper.py
@@ -0,0 +1,11 @@
+#upper() lower() isupper() islower()
+spam = 'Hello, world!'
+print(spam)
+spam = spam.upper()   #spam.upper return a new string, make the origin string be the new string
+print(spam)
+spam = spam.lower()
+print(spam)
+
+print('HELLO is supper: '+str('HELLO'.isupper()))
+print('HELLO is lower: '+str('HELLO'.islower()))
+print('Hello'.upper().lower().upper().upper())
