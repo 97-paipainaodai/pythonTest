6b330238ec4a1dcd05c458247f6f9e45a4e0201d
yss <847830169@qq.com>
2023-02-15 22:40:05 +0800

    [dir]add6_4

---
+++ cht6_String/6_4ord.py
@@ -0,0 +1,10 @@
+#Unicode 
+print(ord('A'))  #65
+print(ord('4'))
+print(ord('!'))
+print(ord('='))
+
+print(chr(65))   #A
+print(chr(ord('A')))  #A
+print(chr(ord('A') + 1))   #B
+print(ord('A') < ord('B'))  #True
